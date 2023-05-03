4d731a0ca859198aea57061e4641d8dae6e36cf9
Davorin <davorin.jurkovic@gmail.com>
2023-05-03 22:48:55 +0200

    First commit

---
+++ PartyApi/PartyApi/PartyExperience.cs
@@ -0,0 +1,13 @@
+﻿namespace PartyApi
+{
+    public class PartyExperience
+    {
+        public DateTime Date { get; set; }
+
+        public string? Location { get; set; }
+
+        public string? Status { get; set; }
+
+        public string? AdditionalInfo { get; set; }
+    }
+}
